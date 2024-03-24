a5d4e16f48535edd2c2d2db10481dc07292a4aa9
Ghost <Ghost>
2024-03-24 20:45:50 +0100

    .

---
+++ l.js
@@ -0,0 +1,60 @@
+/*
+ const cards = [
+  {
+    id: 1,
+    ques:  `hi`,
+    ans: "hello",
+  },
+  {
+    id: 2,
+    ques:  `hi`,
+    ans: "hello",
+  },
+  {
+    id: 3,
+    ques:  `hi`,
+    ans: "hello",
+  },
+  {
+    id: 4,
+    ques:  `hi`,
+    ans: "hello",
+  },
+  {
+    id: 5,
+    ques:  `hi`,
+    ans: "hello",
+  },
+];
+
+const setss = [
+  {
+    name: "1",
+    cards: cards,
+  },
+  {
+    name: "2",
+    cards: cards,
+  },
+  {
+    name: "3",
+    cards: cards,
+  },
+  {
+    name: "4",
+    cards: cards,
+  },
+  {
+    name: "5",
+    cards: cards,
+  },
+];
+
+
+const s =() => {
+    localStorage.setItem("flashcards",JSON.stringify(setss))
+  const store =JSON.parse(localStorage.getItem("flashcards"));
+  console.log(store)
+  return store;
+}
+ const sets = s();*/
